df21b20e1ded71772744b38960a82bee8f06b798
abhilasha2902 <119339978+abhilasha2902@users.noreply.github.com>
2025-02-03 18:46:41 +0530

    Time: 6 ms (87.74%), Space: 44.2 MB (75.65%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -5,13 +5,16 @@ class Solution {
         ArrayList<Integer> dp = new ArrayList<>();
         for (int num : nums) {
             int index = Collections.binarySearch(dp, num); 
+            // it will return negative number if not found  that's why 
+            //down we are checking the condition 
+            //if the num is found did it ,return a negative number  
          if (index < 0) { 
                 index = -(index + 1);
             }
-         if (index == dp.size()) {
+         if (index == dp.size()) { //when num is greater than all element present in dp
                 dp.add(num);
             } else {
-                dp.set(index, num); 
+                dp.set(index, num);  //here we insert the element at the found index in other case than the above one  
             }
         }
          return dp.size(); 
