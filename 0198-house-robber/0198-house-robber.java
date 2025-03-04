4f830d476f0ce6ffd049db0cb65e49edc723f9b0
KUMARI ABHILASHA <119339978+abhilasha2902@users.noreply.github.com>
2025-03-04 11:36:32 +0530

    Time: 0 ms (100%), Space: 41.4 MB (23.54%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -4,10 +4,8 @@ class Solution {
         if (nums.length == 1) return nums[0]; 
          int f = 0, s = 0;
   for (int num : nums) {
-            int temp = f;
-            f = Math.max(f, s + num);
-            s = temp;
+           s = Math.max(f+num, f=s);
         }
-      return f;
+      return s;
     }
 }
