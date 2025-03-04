2451e3cfc072a12b5e636197e007a80a7bed6e78
KUMARI ABHILASHA <119339978+abhilasha2902@users.noreply.github.com>
2025-03-04 10:40:03 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int rob(int[] nums) {
+        // int[] arr= new arr[nums.size];
+        int sum=0;
+        for(int i=0;i<nums.length;i+=2){
+           
+            sum+=nums[i];
+
+        
+    }
+    return sum;
+}
+
+}
